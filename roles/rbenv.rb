c43e5ca82bb2c26e12ab83e34d5ea4fb28d40d9f
bamboo-yujiro <bamboo.yujiro.x@gmail.com>
2020-06-14 13:23:17 +0900

    rbenv のための修正

--- roles/rbenv.rb
+++ roles/rbenv.rb
@@ -1,41 +1,28 @@
+HOME_DIR = "/home/ubuntu"
+RBENV_DIR = "#{HOME_DIR}/.rbenv"
+RBENV_PATH = "#{RBENV_DIR}/bin"
+
 execute 'apt-get update'
 
 %w{ git build-essential libssl-dev libreadline-dev}.each do |pkg|
   package pkg
 end
 
-git "/home/ubuntu/.rbenv" do
+git RBENV_DIR do
   user "ubuntu"
   repository "git://github.com/sstephenson/rbenv.git"
 end
 
-git "/home/ubuntu/.rbenv/plugins/ruby-build" do
+git "#{RBENV_DIR}/plugins/ruby-build" do
   user "ubuntu"
   repository "git://github.com/sstephenson/ruby-build.git"
 end
 
-git "/home/ubuntu/.rbenv/plugins/ruby-update" do
+git "#{RBENV_DIR}/plugins/ruby-update" do
   user "ubuntu"
   repository "git://github.com/rkh/rbenv-update.git"
 end
 
-file '/home/ubuntu/.bashrc' do
-  action :create
-end
-
-file '/home/ubuntu/.bashrc' do
-  action :edit
-  block do |content|
-    next if content.match %r{export PATH="$HOME/.rbenv/bin:$PATH}
-    appendix = <<~EOS
-      # rbenv
-      export PATH="$HOME/.rbenv/bin:$PATH
-      eval "$(rbenv init -)"
-    EOS
-    content << appendix
-  end
-end
-
-execute 'rbenv install 2.3.1'do
+execute "#{RBENV_PATH}/rbenv install 2.3.1" do
   user "ubuntu"
 end
